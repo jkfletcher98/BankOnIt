5e44d05ef09027f8bda47e629648a7aae0a0e089
jkfletcher98 <jkfletcher@bsu.edu>
2025-03-21 16:40:00 +0000

    Issue extending CheckingAccount

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,6 +1,24 @@
 import java.util.*;
 
-class SvaingsAccount extends CheckingAccount {
+class SavingsAccount extends CheckingAccount {
 	double interestRate;
 
+	public static void main(String[] args){
+		SavingsAccount savings = new SavingsAccount();
+		savings.start();
+	} // end main
+
+	public double getInterestRate(){
+		return this.interestRate;
+	} // end getter
+
+	public void getInterestRate(double interestRate){
+		this.interestRate = interestRate;
+	} // end setter
+
+	public void calcInterestRate(){
+		double interest = this.balance * this.interestRate;
+		this.balance += interest;
+	} // end calcInterestRate
+} // end class
 
