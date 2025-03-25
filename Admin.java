cd655f9fb63e6fb11f7ef5545222fe4ad17f9c5b
jkfletcher98 <jkfletcher@bsu.edu>
2025-03-25 15:08:54 +0000

    Strange error

--- Admin.java
+++ Admin.java
@@ -2,14 +2,23 @@ import java.util.*;
 
 class Admin extends User {
 	public Admin(){
+		String userName = "admin";
+		String PIN = "0000";
 	}
 
 	public String menu(){
 		Scanner input = new Scanner(System.in);
-		System.out.println("0) Exit system");
-		System.out.println("1) Admin login");
-		System.out.println("2) Customer login");
+		System.out.println("0) Quit");
+		System.out.println("1) Full customer report");
+		System.out.println("2) Add user");
+		System.out.println("3) Apply interest to savings account");
 		
 		String response = input.nextLine();
 		return response;
 	} // end menu
+
+	public void start(){
+		// will be written in bank
+	} // end start
+
+} // end class
